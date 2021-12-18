05a51ea6c6800a24041a3659af678d2f53bce8f0
Yuriy Bevov <jury19y@mail.ru>
2021-12-18 18:40:50 +0300

    catalog-main

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -6,3 +6,10 @@ import swiper from './modules/swiper.js'
 import menu from './modules/menu.js'
 import filter from './modules/filter.js'
 import catalogMenu from './modules/catalogMenu.js'
+let Masonry = require('masonry-layout');
+
+let msnry = new Masonry( '.cats__grid', {
+    itemSelector: '.cats__grid-item',
+    percentPosition: true,
+    gutter: 16
+  });
