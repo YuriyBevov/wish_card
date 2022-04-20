da231b90eb3acf635fc4e09f7997e9e3736b366c
Yuriy Bevov <jury19y@mail.ru>
2022-04-20 17:25:08 +0300

    catalog

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -16910,77 +16910,24 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-/*const openBtn = document.querySelector('.js-open-catalog-menu');
-const closeBtn = document.querySelector('.js-close-catalog-menu');
-const catalog = document.querySelector('.catalog-menu');
-
-const onClickShowCatalog = () => {
-    catalog.classList.toggle('js-opened');
-}
-
-const onClickHideCatalog = () => {
-    if(catalog.classList.contains('js-opened')) {
-        catalog.classList.remove('js-opened');
-    }
-}
-
-openBtn.addEventListener('click', onClickShowCatalog)
-closeBtn.addEventListener('click', onClickHideCatalog)
-
-const menuItems = document.querySelectorAll('.catalog-menu__item');
-const innerLists = document.querySelectorAll('.catalog-menu__inner');
-
-let isScrolled = false;
-
-const onClickShowInnerList = (evt) => {
-    // evt.preventDefault();
-
-    let elHeight = evt.currentTarget.offsetHeight;
-    let num = evt.currentTarget.getAttribute('data-id');
-
-    let posY = elHeight * (num - 1);
-
-    !evt.currentTarget.classList.contains('active') ?
-    isScrolled = false : null;
-
-    isScrolled !== true ?
-    catalog.scrollTo({
-        top: posY,
-        left: 0,
-        behavior: 'smooth'
-    }) : null;
-
-    isScrolled = true;
-
-    menuItems.forEach(item => {
-        item.classList.contains('active') ?
-        item.classList.remove('active') : null
-    })
-
-    innerLists.forEach(list => {
-        list.classList.contains('active') ?
-        list.classList.remove('active') : null
-    })
-
-    const menuItem = evt.currentTarget;
-
-    menuItem.classList.add('active');
-    menuItem.querySelector('.catalog-menu__inner').classList.add('active')
-}
-
-menuItems.forEach(item => {
-    item.addEventListener('click', onClickShowInnerList)
-})*/
-
-
 const openBtn = document.querySelector('.js-open-catalog-menu');
 const closeBtn = document.querySelector('.js-close-catalog-menu');
 const catalog = document.querySelector('.catalog-menu');
 let body = document.querySelector('body');
+const header = document.querySelector('.header');
+
+let headerHeight = header.getBoundingClientRect().height;
+
+const setCatalogPos = () => {
+    catalog.style.marginTop = headerHeight + 'px';
+    catalog.style.maxHeight = 'calc(100vh - ' + headerHeight + 'px)';
+};
 
 const onClickShowCatalog = () => {
     catalog.classList.toggle('js-opened');
     body.classList.toggle('body-locked');
+
+    setCatalogPos();
 }
 
 const onClickHideCatalog = () => {
@@ -17000,7 +16947,6 @@ let isScrolled = false;
 let windowWidth = window.innerWidth;
 
 const onClickShowInnerList = (evt) => {
-    
     if(windowWidth < 960) {
         let btn = evt.currentTarget.querySelector('button');
 
@@ -17025,10 +16971,6 @@ const onHoverShowInnerList = (evt) => {
             left: 0
         });
 
-        /*let active = evt.currentTarget.querySelector('.catalog-menu__inner');
-
-        active.scrollTo({top: 0}) */
-
         menuItems.forEach(item => {
             item.classList.contains('active') ?
             item.classList.remove('active') : null
@@ -17038,12 +16980,12 @@ const onHoverShowInnerList = (evt) => {
             list.classList.contains('active') ?
             list.classList.remove('active') : null
         })
-
+        
         const menuItem = evt.currentTarget;
-
         menuItem.classList.add('active');
-        menuItem.querySelector('.catalog-menu__inner').classList.add('active')
+        menuItem.querySelector('.catalog-menu__inner').classList.add('active');
     }
+    
 }
 
 
@@ -17054,6 +16996,12 @@ menuItems.forEach(item => {
 
 const onResizeSetWindowWidth = () => {
     windowWidth = window.innerWidth;
+    let currentHeaderHeight = header.getBoundingClientRect().height;
+
+    if(headerHeight !== currentHeaderHeight) {
+        headerHeight = currentHeaderHeight;
+        setCatalogPos();
+    }
 }
 
 window.addEventListener('resize', onResizeSetWindowWidth);
--- source/scripts/modules/catalogMenu.js
+++ source/scripts/modules/catalogMenu.js
@@ -1,74 +1,21 @@
-/*const openBtn = document.querySelector('.js-open-catalog-menu');
-const closeBtn = document.querySelector('.js-close-catalog-menu');
-const catalog = document.querySelector('.catalog-menu');
-
-const onClickShowCatalog = () => {
-    catalog.classList.toggle('js-opened');
-}
-
-const onClickHideCatalog = () => {
-    if(catalog.classList.contains('js-opened')) {
-        catalog.classList.remove('js-opened');
-    }
-}
-
-openBtn.addEventListener('click', onClickShowCatalog)
-closeBtn.addEventListener('click', onClickHideCatalog)
-
-const menuItems = document.querySelectorAll('.catalog-menu__item');
-const innerLists = document.querySelectorAll('.catalog-menu__inner');
-
-let isScrolled = false;
-
-const onClickShowInnerList = (evt) => {
-    // evt.preventDefault();
-
-    let elHeight = evt.currentTarget.offsetHeight;
-    let num = evt.currentTarget.getAttribute('data-id');
-
-    let posY = elHeight * (num - 1);
-
-    !evt.currentTarget.classList.contains('active') ?
-    isScrolled = false : null;
-
-    isScrolled !== true ?
-    catalog.scrollTo({
-        top: posY,
-        left: 0,
-        behavior: 'smooth'
-    }) : null;
-
-    isScrolled = true;
-
-    menuItems.forEach(item => {
-        item.classList.contains('active') ?
-        item.classList.remove('active') : null
-    })
-
-    innerLists.forEach(list => {
-        list.classList.contains('active') ?
-        list.classList.remove('active') : null
-    })
-
-    const menuItem = evt.currentTarget;
-
-    menuItem.classList.add('active');
-    menuItem.querySelector('.catalog-menu__inner').classList.add('active')
-}
-
-menuItems.forEach(item => {
-    item.addEventListener('click', onClickShowInnerList)
-})*/
-
-
 const openBtn = document.querySelector('.js-open-catalog-menu');
 const closeBtn = document.querySelector('.js-close-catalog-menu');
 const catalog = document.querySelector('.catalog-menu');
 let body = document.querySelector('body');
+const header = document.querySelector('.header');
+
+let headerHeight = header.getBoundingClientRect().height;
+
+const setCatalogPos = () => {
+    catalog.style.marginTop = headerHeight + 'px';
+    catalog.style.maxHeight = 'calc(100vh - ' + headerHeight + 'px)';
+};
 
 const onClickShowCatalog = () => {
     catalog.classList.toggle('js-opened');
     body.classList.toggle('body-locked');
+
+    setCatalogPos();
 }
 
 const onClickHideCatalog = () => {
@@ -88,7 +35,6 @@ let isScrolled = false;
 let windowWidth = window.innerWidth;
 
 const onClickShowInnerList = (evt) => {
-    
     if(windowWidth < 960) {
         let btn = evt.currentTarget.querySelector('button');
 
@@ -113,10 +59,6 @@ const onHoverShowInnerList = (evt) => {
             left: 0
         });
 
-        /*let active = evt.currentTarget.querySelector('.catalog-menu__inner');
-
-        active.scrollTo({top: 0}) */
-
         menuItems.forEach(item => {
             item.classList.contains('active') ?
             item.classList.remove('active') : null
@@ -126,12 +68,12 @@ const onHoverShowInnerList = (evt) => {
             list.classList.contains('active') ?
             list.classList.remove('active') : null
         })
-
+        
         const menuItem = evt.currentTarget;
-
         menuItem.classList.add('active');
-        menuItem.querySelector('.catalog-menu__inner').classList.add('active')
+        menuItem.querySelector('.catalog-menu__inner').classList.add('active');
     }
+    
 }
 
 
@@ -142,6 +84,12 @@ menuItems.forEach(item => {
 
 const onResizeSetWindowWidth = () => {
     windowWidth = window.innerWidth;
+    let currentHeaderHeight = header.getBoundingClientRect().height;
+
+    if(headerHeight !== currentHeaderHeight) {
+        headerHeight = currentHeaderHeight;
+        setCatalogPos();
+    }
 }
 
 window.addEventListener('resize', onResizeSetWindowWidth);
